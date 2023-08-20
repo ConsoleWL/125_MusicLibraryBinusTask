9bd3da383570bc42e095b34958caa9747122685d
Nikita Obergan <Obergannikita@gmail.com>
2023-08-19 23:25:52 -0400

    Add project files.

---
+++ 125_MusicLibraryBinusTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using _125_MusicLibraryBinusTask.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace _125_MusicLibraryBinusTask.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs{ get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
